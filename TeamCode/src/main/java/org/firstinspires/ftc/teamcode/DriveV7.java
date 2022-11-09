250874b03f53128b938152c3a47535efe67246b9
LynxLinks <LynxLinksFTC@gmail.com>
2022-11-08 21:43:15 -0900

    v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveV7.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveV7.java
@@ -27,51 +27,51 @@ public class DriveV7 extends LinearOpMode {
     DcMotor M3;
     DcMotor M0_2;
     Servo S0;
+
+    //slide sensor
     DigitalChannel D0;
-    DistanceSensor D1;
-    DistanceSensor D2;
-    DistanceSensor D3;
-    DistanceSensor D4;
-    public static double yoffset = 1;  //constant added to all y positions
-    // distance to cone update from sensor
-    double d = 8.5;
-    //distance strafe at pole but get interupted
-    public static double d2 = 30;
-    //distance come back off of pole
-    public static double d3 = 11;
-    //y offset when coming back
-    public static double d4 = 14;
-    public static double Sset = 200;
-    // average distance
-    double d5 ;
-    //diagonal distance forward and backward
-    boolean track = false;
-    int y = 2;   //y coordinate input
-    int x = 0;   //x coordinate input
-    int ycord = 2;
-    int xcord = 0;
+    DistanceSensor D1; // front
+    DistanceSensor D2; // right
+    DistanceSensor D3; // back
+    DistanceSensor D4; // left
+
+
+    //All d variables
+    double d = 8.5; // distance to pole update from sensor
+    public static double d2 = 30; //distance strafe at pole but get interupted
+    public static double d3 = 11; //distance come back off of pole
+    public static double d4 = 14; //y offset when coming back
+
+    //slide variables
     double target; //slide target position
-    double vy = 1;  //vector roadrunner x value
-    double vx = 1;  //vector roadrunner y value
-    double vo = 1;  //target roadrunner theta
-    double xi = -.5;  //initial robot position against wall in coordinate system, either .5 or -.5
+    public static double Sset = 200; //test drop distance
     int[] hdata = new int[]{200, 1100, 200, 1100, 200,
             1100, 1750, 2350, 1750, 1100,
             200, 2350, 200, 2350, 200,
             1100, 1750, 2350, 1750, 1100,
-            200, 1100, 200, 1100, 200};
+            200, 1100, 200, 1100, 200}; //slide heights
+
+    //RR variable
+    public static double yoffset = 1;  //constant added to all y positions
+    boolean track = false; //update d yes or no
+    int y = 2;   // y finallized when built
+    int x = 0;   // x finallized when built
+    double vy = 1;  //vector roadrunner x value
+    double vx = 1;  //vector roadrunner y value
+    double vo = 1;  //target roadrunner theta
+    double xi = -.5;  //initial robot position against wall in coordinate system, either .5 or -.5
     boolean atwall = true; //used to know whether to run to or from
+
+    //Cordinate variables
+    int ycord = 2; // live cord y
+    int xcord = 0; // live cord x
     boolean dup = false;
     boolean ddown = false;
     boolean dright = false;
     boolean dleft = false;
     boolean dslide = false;
 
-    //distance vars
-    double p = 0;   //position
-    double t = -12; //target
-    double s = 0; //speed
-    double f = 141; //field size
+    //RR trajectories and pose
     Trajectory t1;
     Trajectory t2;
     Trajectory t3;
@@ -82,6 +82,7 @@ public class DriveV7 extends LinearOpMode {
     Pose2d pole;
 
     public void runOpMode() {
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         dashboard = FtcDashboard.getInstance();
         //Add Motors
@@ -97,9 +98,7 @@ public class DriveV7 extends LinearOpMode {
         D4 = hardwareMap.get(DistanceSensor.class, "D4");
         D3 = hardwareMap.get(DistanceSensor.class, "D3");
 
-
         //Set Motors
-
         M0.setDirection(DcMotor.Direction.FORWARD);
         M0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         M1.setDirection(DcMotor.Direction.FORWARD);
@@ -114,7 +113,7 @@ public class DriveV7 extends LinearOpMode {
         M0_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         M0_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
+        //tension slide
         while (D0.getState() == true) {
             M0_2.setPower(.3);
         }
@@ -127,10 +126,15 @@ public class DriveV7 extends LinearOpMode {
         M0_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         M0_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        //open clamp
         S0.setPosition(0.0);
 
+        //waits here
         waitForStart();
+
         target = 200;
+
+        //main loop
         while (opModeIsActive()) {
             Drive();
             ServoClamp();
@@ -140,11 +144,13 @@ public class DriveV7 extends LinearOpMode {
     }
 
     public void ServoClamp() {
-        //automatic clamping if within distance
+        //open cam
         if (gamepad1.left_bumper) {
             S0.setPosition(0.05);
         }
-        if ((target == 200) && (D1.getDistance(DistanceUnit.MM) <= 33) || gamepad1.right_bumper) {
+
+        //automatic clamping if within distance
+        if ((target == 200) && (D1.getDistance(DistanceUnit.MM) <= 40) || gamepad1.right_bumper) {
             target = 0;
             while (Math.abs(target - M0_2.getCurrentPosition()) > 10) {
                 M0_2.setPower(-1 * ((1 - Math.pow(10, ((target - M0_2.getCurrentPosition()) / 250))) / (1 + Math.pow(10, ((target - M0_2.getCurrentPosition()) / 250)))));
@@ -155,7 +161,11 @@ public class DriveV7 extends LinearOpMode {
     }
 
     public void Slide() {
+
+        //slide update equation
         M0_2.setPower(-1 * ((1 - Math.pow(10, ((target - M0_2.getCurrentPosition()) / 250))) / (1 + Math.pow(10, ((target - M0_2.getCurrentPosition()) / 250)))));
+
+        //set position with button
         if (gamepad1.a) {
             target = 200;
             S0.setPosition(0.05);
@@ -163,12 +173,16 @@ public class DriveV7 extends LinearOpMode {
         if (gamepad1.b) target = 2350;
         if (gamepad1.y) target = 1750;
         if (gamepad1.x) target = 1100;
+
+        //slide reset if at switch
         if (D0.getState() && (target == 0)) {
             M0_2.setDirection(DcMotor.Direction.FORWARD);
             M0_2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             M0_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             M0_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+
+        //test drop onto pole
         if (gamepad1.dpad_down) dslide = true;
         if ((!gamepad1.dpad_down) && dslide) {
             dslide = false;
@@ -177,9 +191,9 @@ public class DriveV7 extends LinearOpMode {
     }
 
 
-
     public void Drive() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        //standard drive
         double yAxis;
         double xAxis;
         double Rotate;
@@ -191,8 +205,13 @@ public class DriveV7 extends LinearOpMode {
         M1.setPower(-(Rotate + (yAxis + xAxis)));
         M2.setPower(-(Rotate + (yAxis - xAxis)));
 
+        //Automated Cordiante system
         if (gamepad1.right_stick_button) {
+
+            //going to pole movement
             if (atwall) {
+
+                //set varibles based off of cordinates
                 y = ycord;
                 x = xcord;
                 vy = -(yoffset + 24 * (y - 1));
@@ -214,6 +233,7 @@ public class DriveV7 extends LinearOpMode {
                     d2 = -d2;
                 }
 
+                //build non x translation
                 if(vx == 0){
                     t1 = drive.trajectoryBuilder(new Pose2d(0,0,0))
                             .back(-(vy-d2))
@@ -222,6 +242,8 @@ public class DriveV7 extends LinearOpMode {
                             })
                             .build();
                 }
+
+                //build with x translation
                 else{
                     t1 = drive.trajectoryBuilder(new Pose2d(0,0,0))
                             .lineToLinearHeading(new Pose2d(vy, 0, Math.toRadians(vo)))
@@ -229,16 +251,18 @@ public class DriveV7 extends LinearOpMode {
                                 drive.followTrajectoryAsync(t2);
                             })
                             .build();
-                    //move to x position
                     t2 = drive.trajectoryBuilder(t1.end())
                             .lineToLinearHeading(new Pose2d(vy, vx, Math.toRadians(vo)))
                             .build();
-                    //move diagonal forwards to target junction
-                    //move diagonal backwards to center of tile
                 }
+
+                //close clamp
                 S0.setPosition(.3);
+
+                //retrieve height of pole
                 target = hdata[x + 5*(y-1)+2];
 
+                //movement 1 with distance interrupt
                 drive.followTrajectoryAsync(t1);
                 drive.update();
                 while (Math.abs(gamepad1.left_stick_x) < .5
@@ -261,12 +285,14 @@ public class DriveV7 extends LinearOpMode {
                         M2.setPower(0);
                     }
                 }
+
+                //movement 2 build
                 drive.setPoseEstimate(new Pose2d());
                 t4 = drive.trajectoryBuilder(new Pose2d(2,0,Math.toRadians(vo)))
                         .lineToLinearHeading(new Pose2d(2,d+2,Math.toRadians(vo)))
                         .build();
 
-
+                //movement 2 onto pole
                 drive.followTrajectoryAsync(t4);
                 drive.update();
                 while (Math.abs(gamepad1.left_stick_x) < .5
@@ -287,9 +313,14 @@ public class DriveV7 extends LinearOpMode {
                 }
                 atwall = false;
             }
+
+            //movement returning to wall
             else {
+                //set pose at
                 pole = new Pose2d(vy - d4,vx + Math.sin(vo)*d3 ,Math.toRadians(vo));
                 drive.setPoseEstimate(pole);
+
+                //neeeeeeds to be fixed but build back
                 f1 = drive.trajectoryBuilder(pole)
                         .back(d3)
                         .addDisplacementMarker(() -> {
@@ -316,8 +347,11 @@ public class DriveV7 extends LinearOpMode {
                             .build();
 
                 }
+
+                //open cam
                 S0.setPosition(0);
 
+                //return movement
                 drive.followTrajectoryAsync(f1);
                 drive.update();
                 while (Math.abs(gamepad1.left_stick_x) < .5
@@ -342,17 +376,24 @@ public class DriveV7 extends LinearOpMode {
         }
 
     }
+
     public void Coordinates () {
+
+        //toggle cords
         if (gamepad2.dpad_up) dup = true;
         if (gamepad2.dpad_down) ddown = true;
         if (gamepad2.dpad_left) dleft = true;
         if (gamepad2.dpad_right) dright = true;
+
+        //field inital position
         if (gamepad2.right_bumper) {
             xi = .5;
         }
         if (gamepad2.left_bumper) {
             xi = -.5;
         }
+
+        //set game field cordinates
         if ((!gamepad2.dpad_up) && dup) {
             dup = false;
             ycord += 1;
@@ -369,6 +410,8 @@ public class DriveV7 extends LinearOpMode {
             dleft = false;
             xcord -= 1;
         }
+
+        //all telemetry
         telemetry.addData("x", xcord);
         telemetry.addData("y", ycord);
         telemetry.addData("xi", xi);
@@ -378,10 +421,6 @@ public class DriveV7 extends LinearOpMode {
         telemetry.addData("left", D4.getDistance(DistanceUnit.INCH));
         telemetry.addData("d",d);
         telemetry.addData("track",track);
-
-        //telemetry.addData("right", D2.getDistance(DistanceUnit.MM));
-        //telemetry.addData("left", D4.getDistance(DistanceUnit.MM));
-
         telemetry.update();
 
     }
